f45a11f7b049296776cea80bd178fb219775f072
Ian Schafer <ianschafer@gmail.com>
2018-05-30 23:51:31 +0200

    Journal App
    File I/O, Lists, Multiple files, __name__ (dundername), complex conditionals, for-in loops, doc strings, live templates, file path management, importing modules and packages, os package (os.path), using context manager "with" to close file and cleanup

---
+++ App4_journal/program2.py
@@ -0,0 +1,14 @@
+print("1. About to import main")
+
+# print("__file__ " + __file__)
+# print("__name__ " + __name__)
+
+import main
+
+print("2. main imported")
+
+print("3. Printing header!")
+main.print_header()
+
+print("4. Done with main.")
+
