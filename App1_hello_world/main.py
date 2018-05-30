36e4875872b11ff8a08b91b52425f70506d26254
Ian Schafer <ianschafer@gmail.com>
2018-05-30 21:10:54 +0200

    Very basic - print()

---
+++ App1_hello_world/main.py
@@ -0,0 +1,9 @@
+print('--------------------------------')
+print('          HELLO Ian')
+print('--------------------------------')
+print()
+
+user_input = input('What is your name? ')
+message = 'Nice to meet you ' + user_input
+
+print(message)
