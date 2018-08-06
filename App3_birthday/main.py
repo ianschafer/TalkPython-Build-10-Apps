45b28b93e653a67eeb0a5ba7850f52dcbdfea37c
Ian Schafer <ianschafer@gmail.com>
2018-08-06 21:01:10 +0200

    Update main.py

--- App3_birthday/main.py
+++ App3_birthday/main.py
@@ -27,7 +27,7 @@ def compute_days_between_dates(birth_date, target_date):
 
 def print_birthday_information(days):
     if days < 0:
-        print("You had your birthday {} days ago this year.".format(-days))
+        print("Your birthday was {} days ago this year.".format(-days))
     elif days > 0:
         print("Your birthday is in {} days!".format(days))
     else:
